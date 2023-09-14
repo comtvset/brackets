675a364a8c5b11f3ca316ff0a0cc724aa0135851
Vadzim Tsikhaniuk <comtvset@gmail.com>
2023-09-15 01:29:55 +0200

    fix: JS: Brackets is done (Fri, Sep 15, 2023 01:29:55 AM)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const OPEN = ['(', '[', '{', '1', '3', '5', '7', '8', '|'];
+  const CLOSE = {
+    [')']: '(',
+    [']']: '[',
+    ['}']: '{',
+    ['2']: '1',
+    ['4']: '3',
+    ['6']: '5',
+    ['7']: 'open7',
+    ['8']: 'open8',
+    ['|']: 'open|',
+  };
+
+  let stack = [];
+  let newStr = str.split('');
+
+  for (let i = 0; i < newStr.length; i++) {
+    let cur = newStr[i];
+
+    if (OPEN.includes(cur)) {
+      if(cur === '|') {
+        if(stack.includes('open|')) {
+          stack.pop();
+        } else {stack.push('open|')}
+      }
+      else if(cur === '7') {
+        if(stack.includes('open7')) {
+          stack.pop();
+        } else {stack.push('open7')}
+      }
+      else if(cur === '8') {
+        if(stack.includes('open8')) {
+          stack.pop();
+        } else {stack.push('open8')}
+      }
+      else {stack.push(cur)}
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+
+      let topEl = stack[stack.length - 1];
+
+      if (CLOSE[cur] === topEl) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
